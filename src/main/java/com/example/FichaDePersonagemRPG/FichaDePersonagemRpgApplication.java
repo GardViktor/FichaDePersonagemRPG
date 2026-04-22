8af9b640a483ab1c55e28b1b2249f0b7cf3ea49d
Edgard <edgardvictorlol@gmail.com>
2026-04-22 13:06:26 -0300

    first commit

---
+++ src/main/java/com/example/FichaDePersonagemRPG/FichaDePersonagemRpgApplication.java
@@ -0,0 +1,13 @@
+package com.example.FichaDePersonagemRPG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichaDePersonagemRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichaDePersonagemRpgApplication.class, args);
+	}
+
+}
